68763e399c8b3ed4b41b9dcae546e8002223289a
EdberMartins <edbergmartins@gmail.com>
2022-02-06 14:35:00 -0300

    verificacoes de seguranca para preenchimento de campos

--- scripts.js
+++ scripts.js
@@ -72,3 +72,31 @@ btn.addEventListener("click", function (e) {
 
 
 });
+
+
+function checkInputs(inputs) {
+  var filled = true;
+  
+  inputs.forEach(function(input) {
+      
+    if(input.value === "") {
+        filled = false;
+    }
+  
+  });
+  
+  return filled;
+  
+}
+var inputs = document.querySelectorAll("input");
+var button = document.getElementById("send");
+inputs.forEach(function(input) {
+    
+  input.addEventListener("keyup", function() {
+    if(checkInputs(inputs)) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  });
+});
